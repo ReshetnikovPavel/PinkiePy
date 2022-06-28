723f0532387e485c4b9728f6bdbed465e64eaa53
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2022-06-28 23:09:27 +0500

    Now lexer works and looks a bit cleaner

--- lexer.py
+++ lexer.py
@@ -5,98 +5,151 @@ def regex_is_keyword():
     return ''.join([rf'({key})|' for key in keywords])[:-1]
 
 
+def infix(key):
+    return rf"(?<=[\s.,!?‽…:]){key}(?=[\s.,!?‽…:])"
+
+
+def add_not_postfix(key):
+    return rf"{key}(?:(?:n't)|(?: not))"
+
+
 keywords = {
     r"\d+": "NUMBER",
-    r'["”“].*?["”“]': "STRING",
+    r'["”“](?:.|\n)*?["”“]': "STRING",
     r"['‘’].?['‘’]": "CHAR",
     r"[.,!?‽…:]": "PUNCTUATION",
-    r"(P.)+S.+\n": "COMMENT_INLINE",
-    r"\(.*\)": "COMMENT_BLOCK",
+    #r"(P.)+S.+\n": "COMMENT_INLINE",
+    #r"\(.*\)": "COMMENT_BLOCK",
     r"/": "SOLIDUS",
 
     r"That's what I would do": "END",
     r"Conditional conclusion": "DEFAULT",
-    r"(Or else)|(Otherwise)": "ELSE",
-    r"On the .+((nd)|(rd)|(st)|(th))? hoof": "CASE",
-    r"(If)|(When) .+( then)?": "IF",
+    r"Or else": "ELSE",
+    r"Otherwise": "ELSE",
+    r"On the": "CASE_PREFIX",
+    r"(?:(?:nd)|(?:rd)|(?:st)|(?:th))? hoof": "CASE_POSTFIX",
+    r"If": "IF_PREFIX",
+    r"When": "IF_PREFIX",
+    r"then": "IF_POSTFIX",
     r"In regards to": "SWITCH",
     r"Dear": "REPORT_DECLARATION",
     r"I learned": "PARAGRAPH_DECLARATION",
     r"Today I learned": "MANE_PARAGRAPH_DECLARATION",
     r"Did you know that": "VARIABLE_DEFINITION",
-    r"(to get)|(with)": "RETURNED_VARIABLE_TYPE_DEFINITION",
+    r"to get": "RETURNED_VARIABLE_TYPE_DEFINITION",
+    r"with": "RETURNED_VARIABLE_TYPE_DEFINITION",
     r"Your faithful student": "END_REPORT",
     r"That’s all about": "END_PARAGRAPH",
     r"Remember when I wrote about": "REPORT_IMPORT",
-    r"(has)|(is)|(likes?)|(was)": "VARIABLE_INITIALIZATION",
-    r"always (has)|(is)|(likes?)|(was)": "CONSTANT_INITIALIZATION",
-    r"Dear .+ and": "LISTING_IMPORTED_INTERFACES",
+    rf"{infix('has')}": "VARIABLE_INITIALIZATION-EQUAL_INFIX",
+    rf"{infix('is')}": "VARIABLE_INITIALIZATION-EQUAL_INFIX",
+    r"likes?": "VARIABLE_INITIALIZATION",
+    rf"{infix('was')}": "VARIABLE_INITIALIZATION-EQUAL_INFIX",
+    r"always": "CONSTANT_INITIALIZATION",
     r"using": "LISTING_PARAGRAPH_PARAMETERS",
-    r"I did this (while)|(as long as)": "END_DO_WHILE",
+    r"I did this while": "END_DO_WHILE",
+    r"I did this as long as": "END_DO_WHILE",
     r"That's what I did": "END_WHILE",
     r"Here's what I did": "DO_WHILE",
-    r"(As long as)|(while)": "WHILE",
-    r"I (said)|(sang)|(wrote)": "PRINT",
+    r"As long as": "WHILE",
+    r"While": "WHILE",
+    r"I said": "PRINT",
+    r"I sang": "PRINT",
+    r"I wrote": "PRINT",
     r"I asked": "PROMPT_USER_FOR_IMPORT",
-    r"I (heard)|(read)": "READLINE",
+    r"I heard": "READLINE",
+    r"I read": "READLINE",
     r"Then you get": "RETURN",
-    r"I (remembered)|(would)": "RUN_PARAGRAPH",
-    r"((an )|(the )?argument)|((the )?logic)": "BOOLEAN_TYPE",
-    r"(the )?(arguments)|(logics)": "BOOLEAN_ARRAY_TYPE",
-    r"(a )|(the )?number": "NUMBER_64_TYPE",
-    r"(a )|(the )?(character)|(letter)": "CHAR_TYPE",
-    r"(many )|(the )?numbers": "NUMBER_64_ARRAY_TYPE",
-    r"(many )|(the )?(phrases)|(quotes)|(sentences)|(words)":
-        "CHAR_ARRAY_OF_ARRAYS_TYPE",
-    r"((a )|(the )?(phrase)|(quote)|(sentence)|(word))"
-    r"|((the )?(characters)|(letters))": "CHAR_ARRAY_TYPE",
-    r"(are now)|(becomes?)|(is now)|(now likes?)": "VARIABLE_VALUE_ASSIGNMENT",
+    r"I remembered": "RUN_PARAGRAPH",
+    r"I would": "RUN_PARAGRAPH",
+    r"(?:the )?logic": "BOOLEAN_TYPE",
+    r"(?:(?:an )|(?:the ))?argument": "BOOLEAN_TYPE",
+    r"(?:the )?logics": "BOOLEAN_ARRAY_TYPE",
+    r"(?:(?:a )|(?:the ))?number": "NUMBER_64_TYPE",
+    r"(?:(?:a )|(?:the ))?character": "CHAR_TYPE",
+    r"(?:(?:a )|(?:the ))?letter": "CHAR_TYPE",
+    r"(?:(?:many )|(?:the ))?numbers": "NUMBER_64_ARRAY_TYPE",
+    r"(?:(?:many )|(?:the ))?phrases": "CHAR_ARRAY_OF_ARRAYS_TYPE",
+    r"(?:(?:many )|(?:the ))?quotes": "CHAR_ARRAY_OF_ARRAYS_TYPE",
+    r"(?:(?:many )|(?:the ))?sentences": "CHAR_ARRAY_OF_ARRAYS_TYPE",
+    r"(?:(?:many )|(?:the ))?words": "CHAR_ARRAY_OF_ARRAYS_TYPE",
+    r"(?:(?:a )|(?:the ))?phrase": "CHAR_ARRAY_TYPE",
+    r"(?:(?:a )|(?:the ))?quote": "CHAR_ARRAY_TYPE",
+    r"(?:(?:a )|(?:the ))?sentence": "CHAR_ARRAY_TYPE",
+    r"(?:(?:a )|(?:the ))?word": "CHAR_ARRAY_TYPE",
+    r"(?:the )?characters": "CHAR_ARRAY_TYPE",
+    r"(?:the )?letters": "CHAR_ARRAY_TYPE",
+    r"are now": "VARIABLE_VALUE_ASSIGNMENT",
+    r"becomes?": "VARIABLE_VALUE_ASSIGNMENT",
+    r"is now": "VARIABLE_VALUE_ASSIGNMENT",
+    r"now likes?": "VARIABLE_VALUE_ASSIGNMENT",
     #   Operators
-    r"(added to)|(and)|(plus)": "ADDITION_INFIX",
-    r"divided by": "DIVISION_INFIX",
-    r"(multiplied with)|(times)": "MULTIPLICATION_INFIX",
-    r"(minus)|(without)": "SUBTRACTION_INFIX",
+    rf"{infix('added to')}": "ADDITION_INFIX",
+    rf"{infix('plus')}": "ADDITION_INFIX",
+    rf"{infix('divided by')}": "DIVISION_INFIX",
+    rf"{infix('multiplied with')}": "MULTIPLICATION_INFIX",
+    rf"{infix('times')}": "MULTIPLICATION_INFIX",
+    rf"{infix('minus')}": "SUBTRACTION_INFIX",
+    rf"{infix('without')}": "SUBTRACTION_INFIX",
     r"got one less": "DECREMENT_POSTFIX",
     r"got one more": "INCREMENT_POSTFIX",
-    r"add .+ and": "ADDITION_PREFIX",
-    r"divide .+ (and)|(by)": "DIVISION_PREFIX",
-    r"(multiply)|(the product of) .+ (and)|(by)": "MULTIPLICATION_PREFIX",
-    r"(subtract)|(the difference between) .+ (and)|(from)":
-        "SUBTRACTION_PREFIX",
+    r"add": "ADDITION_PREFIX",
+    r"divide": "DIVISION_PREFIX",
+    r"by": "BY",
+    r"multiply": "MULTIPLICATION_PREFIX",
+    r"the product of": "MULTIPLICATION_PREFIX",
+    r"subtract": "SUBTRACTION_PREFIX",
+    r"the difference between": "SUBTRACTION_PREFIX",
+    r"from": "FROM",
     r"There was one less": "DECREMENT_PREFIX",
     r"There was one more": "INCREMENT_PREFIX",
-    r"and": "AND_INFIX",
-    r"or": "OR_INFIX",
-    r"either .+ or": "EXCLUSIVE_OR_PREFIX",
-    r"(not)|(it's not the case that)": "NOT_PREFIX",
-    r"(had)|(has)|(is)|(was)|(were)": "EQUAL_INFIX",
-    r"(had)|(has)|(is)|(was)|(were)(n't)|( not)": "NOT_EQUAL_INFIX",
-    r"(((is)|(was)|(were) (greater)|(more))|((had)|(has) more)) than":
+    rf"{infix('and')}": "AND_INFIX",
+    rf"{infix('or')}": "OR_INFIX",
+    r"either": "EXCLUSIVE_OR_PREFIX",
+    r"not": "NOT_PREFIX",
+    r"it's not the case that": "NOT_PREFIX",
+    rf"{infix('(?:(?:had)|(?:has)) more than')}": "GREATER_THAN_INFIX",
+    rf"{infix('(?:(?:is)|(?:was)|(?:were)) greater than')}":
         "GREATER_THAN_INFIX",
+    rf"{infix('(?:(?:is)|(?:was)|(?:were)) more than')}":
+        "GREATER_THAN_INFIX",
+    #   TODO
     r"(((is)|(was)|(were)(n't)|( not)|( no))|((had)|(has) no)) less than":
         "GREATER_THAN_OR_EQUAL_INFIX",
-    r"(had)|(has)|(is)|(was)|(were) less than": "LESS_THAN_INFIX",
-    r"((is)|(was)|(were)(n't)|( not)|( no) (greater)|(more))"
-    r"|((had)|(has) no more) than":
+    rf"{infix('(?:(?:had)|(?:has)) less than')}": "LESS_THAN_INFIX",
+    rf"{infix('(?:(?:is)|(?:was)|(?:were)) less than')}":
+        "LESS_THAN_INFIX",
+    #   TODO
+    r"(?<=[\s.,!?‽…:])((is)|(was)|(were)(n't)|( not)|( no) (greater)|(more))"
+    r"|((had)|(has) no more) than(?=[\s.,!?‽…:])":
         "GREATER_THAN_OR_EQUAL_INFIX",
-    #   Literals
-    r"(correct)|(right)|(true)|(yes)": "TRUE",
-    r"(false)|(incorrect)|(no)|(wrong)": "FALSE",
+    rf"{infix('had')}": "EQUAL_INFIX",
+    rf"{infix('were')}": "EQUAL_INFIX",
+    rf"{infix(add_not_postfix('had'))}": "NOT_EQUAL_INFIX",
+    rf"{infix(add_not_postfix('has'))}": "NOT_EQUAL_INFIX",
+    rf"{infix(add_not_postfix('is'))}": "NOT_EQUAL_INFIX",
+    rf"{infix(add_not_postfix('was'))}": "NOT_EQUAL_INFIX",
+    rf"{infix(add_not_postfix('were'))}": "NOT_EQUAL_INFIX",
+
+    r"correct": "TRUE",
+    r"right": "TRUE",
+    r"true": "TRUE",
+    r"yes": "TRUE",
+    r"false": "FALSE",
+    r"incorrect": "FALSE",
+    r"no": "FALSE",
+    r"wrong": "FALSE",
     r"nothing": "NULL",
-
-
-    #r"[a-zA-z_][a-zA-Z0-9_\s']*": "NAME",
-    #r"\s+": "WHITESPACE",
-    #r".+?": "NONSENSE"
 }
-
-keywords[rf"(?:(?!{regex_is_keyword()}).)*"] = "NAME"
-keywords[r".+?"] = "NONSENSE"
 
 lg = LexerGenerator()
 lg.ignore(r"\s")
+lg.ignore(r"(P.)+S.+\n")
+lg.ignore(r"\(.*\)")
 for keyword in keywords.items():
     lg.add(keyword[1], keyword[0])
+lg.add("NAME", rf'(?:(?!{regex_is_keyword()}).)*')
+lg.add("NONSENSE", r".+?")
 lexer = lg.build()
 
 program = """Dear Princess Celestia: Hello World!
