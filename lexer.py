39ca4331ac7595e11f5beb8a469a41fddd51e3c6
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2022-06-30 01:54:55 +0500

    more fixes (+unuseful keyword_builer)

--- lexer.py
+++ lexer.py
@@ -1,154 +1,166 @@
 from rply import LexerGenerator
+import re
 
 
-def regex_is_keyword():
-    return ''.join([rf'({key})|' for key in keywords])[:-1]
+def regex_is_in(iterator):
+    return ''.join([rf'({key[0]})|' for key in iterator])[:-1]
 
 
 def infix(key):
-    return rf"(?<=[\s.,!?‽…:]){key}(?=[\s.,!?‽…:])"
+    return rf"(?<=\s){key}(?=\s)"
 
 
 def add_not_postfix(key):
     return rf"{key}(?:(?:n't)|(?: not))"
 
 
-keywords = {
-    r"\d+": "NUMBER",
-    r'["”“](?:.|\n)*?["”“]': "STRING",
-    r"['‘’].?['‘’]": "CHAR",
-    r"[.,!?‽…:]": "PUNCTUATION",
-    #r"(P.)+S.+\n": "COMMENT_INLINE",
-    #r"\(.*\)": "COMMENT_BLOCK",
-    r"/": "SOLIDUS",
+def add_boundary(regex_expr):
+    return rf"\b{regex_expr}\b"
 
-    r"That's what I would do": "END",
-    r"Conditional conclusion": "DEFAULT",
-    r"Or else": "ELSE",
-    r"Otherwise": "ELSE",
-    r"On the": "CASE_PREFIX",
-    r"(?:(?:nd)|(?:rd)|(?:st)|(?:th))? hoof": "CASE_POSTFIX",
-    r"If": "IF_PREFIX",
-    r"When": "IF_PREFIX",
-    r"then": "IF_POSTFIX",
-    r"In regards to": "SWITCH",
-    r"Dear": "REPORT_DECLARATION",
-    r"I learned": "PARAGRAPH_DECLARATION",
-    r"Today I learned": "MANE_PARAGRAPH_DECLARATION",
-    r"Did you know that": "VARIABLE_DEFINITION",
-    r"to get": "RETURNED_VARIABLE_TYPE_DEFINITION",
-    r"with": "RETURNED_VARIABLE_TYPE_DEFINITION",
-    r"Your faithful student": "END_REPORT",
-    r"That’s all about": "END_PARAGRAPH",
-    r"Remember when I wrote about": "REPORT_IMPORT",
-    rf"{infix('has')}": "VARIABLE_INITIALIZATION-EQUAL_INFIX",
-    rf"{infix('is')}": "VARIABLE_INITIALIZATION-EQUAL_INFIX",
-    r"likes?": "VARIABLE_INITIALIZATION",
-    rf"{infix('was')}": "VARIABLE_INITIALIZATION-EQUAL_INFIX",
-    r"always": "CONSTANT_INITIALIZATION",
-    r"using": "LISTING_PARAGRAPH_PARAMETERS",
-    r"I did this while": "END_DO_WHILE",
-    r"I did this as long as": "END_DO_WHILE",
-    r"That's what I did": "END_WHILE",
-    r"Here's what I did": "DO_WHILE",
-    r"As long as": "WHILE",
-    r"While": "WHILE",
-    r"I said": "PRINT",
-    r"I sang": "PRINT",
-    r"I wrote": "PRINT",
-    r"I asked": "PROMPT_USER_FOR_IMPORT",
-    r"I heard": "READLINE",
-    r"I read": "READLINE",
-    r"Then you get": "RETURN",
-    r"I remembered": "RUN_PARAGRAPH",
-    r"I would": "RUN_PARAGRAPH",
-    r"(?:the )?logic": "BOOLEAN_TYPE",
-    r"(?:(?:an )|(?:the ))?argument": "BOOLEAN_TYPE",
-    r"(?:the )?logics": "BOOLEAN_ARRAY_TYPE",
-    r"(?:(?:a )|(?:the ))?number": "NUMBER_64_TYPE",
-    r"(?:(?:a )|(?:the ))?character": "CHAR_TYPE",
-    r"(?:(?:a )|(?:the ))?letter": "CHAR_TYPE",
-    r"(?:(?:many )|(?:the ))?numbers": "NUMBER_64_ARRAY_TYPE",
-    r"(?:(?:many )|(?:the ))?phrases": "CHAR_ARRAY_OF_ARRAYS_TYPE",
-    r"(?:(?:many )|(?:the ))?quotes": "CHAR_ARRAY_OF_ARRAYS_TYPE",
-    r"(?:(?:many )|(?:the ))?sentences": "CHAR_ARRAY_OF_ARRAYS_TYPE",
-    r"(?:(?:many )|(?:the ))?words": "CHAR_ARRAY_OF_ARRAYS_TYPE",
-    r"(?:(?:a )|(?:the ))?phrase": "CHAR_ARRAY_TYPE",
-    r"(?:(?:a )|(?:the ))?quote": "CHAR_ARRAY_TYPE",
-    r"(?:(?:a )|(?:the ))?sentence": "CHAR_ARRAY_TYPE",
-    r"(?:(?:a )|(?:the ))?word": "CHAR_ARRAY_TYPE",
-    r"(?:the )?characters": "CHAR_ARRAY_TYPE",
-    r"(?:the )?letters": "CHAR_ARRAY_TYPE",
-    r"are now": "VARIABLE_VALUE_ASSIGNMENT",
-    r"becomes?": "VARIABLE_VALUE_ASSIGNMENT",
-    r"is now": "VARIABLE_VALUE_ASSIGNMENT",
-    r"now likes?": "VARIABLE_VALUE_ASSIGNMENT",
-    #   Operators
-    rf"{infix('added to')}": "ADDITION_INFIX",
-    rf"{infix('plus')}": "ADDITION_INFIX",
-    rf"{infix('divided by')}": "DIVISION_INFIX",
-    rf"{infix('multiplied with')}": "MULTIPLICATION_INFIX",
-    rf"{infix('times')}": "MULTIPLICATION_INFIX",
-    rf"{infix('minus')}": "SUBTRACTION_INFIX",
-    rf"{infix('without')}": "SUBTRACTION_INFIX",
-    r"got one less": "DECREMENT_POSTFIX",
-    r"got one more": "INCREMENT_POSTFIX",
-    r"add": "ADDITION_PREFIX",
-    r"divide": "DIVISION_PREFIX",
-    r"by": "BY",
-    r"multiply": "MULTIPLICATION_PREFIX",
-    r"the product of": "MULTIPLICATION_PREFIX",
-    r"subtract": "SUBTRACTION_PREFIX",
-    r"the difference between": "SUBTRACTION_PREFIX",
-    r"from": "FROM",
-    r"There was one less": "DECREMENT_PREFIX",
-    r"There was one more": "INCREMENT_PREFIX",
-    rf"{infix('and')}": "AND_INFIX",
-    rf"{infix('or')}": "OR_INFIX",
-    r"either": "EXCLUSIVE_OR_PREFIX",
-    r"not": "NOT_PREFIX",
-    r"it's not the case that": "NOT_PREFIX",
-    rf"{infix('(?:(?:had)|(?:has)) more than')}": "GREATER_THAN_INFIX",
-    rf"{infix('(?:(?:is)|(?:was)|(?:were)) greater than')}":
-        "GREATER_THAN_INFIX",
-    rf"{infix('(?:(?:is)|(?:was)|(?:were)) more than')}":
-        "GREATER_THAN_INFIX",
-    #   TODO
-    r"(((is)|(was)|(were)(n't)|( not)|( no))|((had)|(has) no)) less than":
-        "GREATER_THAN_OR_EQUAL_INFIX",
-    rf"{infix('(?:(?:had)|(?:has)) less than')}": "LESS_THAN_INFIX",
-    rf"{infix('(?:(?:is)|(?:was)|(?:were)) less than')}":
-        "LESS_THAN_INFIX",
-    #   TODO
-    r"(?<=[\s.,!?‽…:])((is)|(was)|(were)(n't)|( not)|( no) (greater)|(more))"
-    r"|((had)|(has) no more) than(?=[\s.,!?‽…:])":
-        "GREATER_THAN_OR_EQUAL_INFIX",
-    rf"{infix('had')}": "EQUAL_INFIX",
-    rf"{infix('were')}": "EQUAL_INFIX",
-    rf"{infix(add_not_postfix('had'))}": "NOT_EQUAL_INFIX",
-    rf"{infix(add_not_postfix('has'))}": "NOT_EQUAL_INFIX",
-    rf"{infix(add_not_postfix('is'))}": "NOT_EQUAL_INFIX",
-    rf"{infix(add_not_postfix('was'))}": "NOT_EQUAL_INFIX",
-    rf"{infix(add_not_postfix('were'))}": "NOT_EQUAL_INFIX",
 
-    r"correct": "TRUE",
-    r"right": "TRUE",
-    r"true": "TRUE",
-    r"yes": "TRUE",
-    r"false": "FALSE",
-    r"incorrect": "FALSE",
-    r"no": "FALSE",
-    r"wrong": "FALSE",
-    r"nothing": "NULL",
-}
+def _sort():
+    return sorted(
+        keywords,
+        reverse=True,
+        key=lambda x: (len(re.findall(r"[A-Za-z)?]\s[A-Za-z(]", x[0])),
+                       len(x[0])))
+
+
+literals = [(r'["”“](?:.|\n)*?["”“]', "STRING"),
+            (r"['‘’].?['‘’]", "CHAR"),
+            (r"\d+", "NUMBER"),
+            (r"[.,!?‽…:]", "PUNCTUATION"),
+            # r"(P.)+S.+\n": "COMMENT_INLINE",
+            # r"\(.*\)": "COMMENT_BLOCK",
+            (r"/", "SOLIDUS"),
+            (r'\bcorrect\b', 'TRUE'),
+            (r'\bright\b', 'TRUE'),
+            (r'\btrue\b', 'TRUE'),
+            (r'\byes\b', 'TRUE'),
+            (r'\bincorrect\b', 'FALSE'),
+            (r'\bfalse', 'FALSE'),
+            (r'\bwrong\b', 'FALSE'),
+            (r'\bno\b', 'FALSE'),
+            (r'\bnothing\b', 'NULL'),
+            ]
+
+keywords = [
+            (r"[.,!?‽…:]", "PUNCTUATION"),
+            (r'I did this as long as', 'END_DO_WHILE'),
+            (r"it's not the case that", 'NOT_PREFIX'),
+            (r'(?:(?:is)|(?:was)|(?:were)|(?:had)|(?:has)) no more than',
+             'LESS_THAN_OR_EQUAL_INFIX'),
+            (r'(?:(?:had)|(?:has)) no less than', 'GREATER_THAN_OR_EQUAL_INFIX'),
+            (r'Remember when I wrote about', 'REPORT_IMPORT'),
+            (r"That's what I would do", 'END'),
+            (r'Did you know that', 'VARIABLE_DEFINITION'),
+            (r'I did this while', 'END_DO_WHILE'),
+            (r"(?:(?:is)|(?:was)|(?:were))(?:(?:n't)|(?: not)) greater than",
+             'LESS_THAN_OR_EQUAL_INFIX'),
+            (r"(?:(?:is)|(?:was)|(?:were))(?:(?:n't)|(?: not)) less than",
+             'GREATER_THAN_OR_EQUAL_INFIX'),
+            (r'(?:(?:is)|(?:was)|(?:were)) greater than', 'GREATER_THAN_INFIX'),
+            (r'(?:(?:is)|(?:was)|(?:were)) more than', 'GREATER_THAN_INFIX'),
+            (r'(?:(?:is)|(?:was)|(?:were)) less than', 'LESS_THAN_INFIX'),
+            (r'(?:(?:had)|(?:has)) more than', 'GREATER_THAN_INFIX'),
+            (r'(?:(?:had)|(?:has)) less than', 'LESS_THAN_INFIX'),
+            (r'the difference between(?=.+?)(?![.,!?‽…:])(?=.+?)(?:(?:and)|(?:from))', 'SUBTRACTION_PREFIX'),
+            (r'Your faithful student', 'END_REPORT'),
+            (r'There was one less', 'DECREMENT_PREFIX'),
+            (r'There was one more', 'INCREMENT_PREFIX'),
+            (r"That's what I did", 'END_WHILE'),
+            (r"Here's what I did", 'DO_WHILE'),
+            (r'That’s all about', 'END_PARAGRAPH'),
+            (r'In regards to', 'SWITCH'),
+            (r'got one less', 'DECREMENT_POSTFIX'),
+            (r'got one more', 'INCREMENT_POSTFIX'),
+            (r'Then you get', 'RETURN'),
+            (r'As long as', 'WHILE'),
+            (r'(?:(?:nd)|(?:rd)|(?:st)|(?:th))? hoof', 'CASE_POSTFIX'),
+            (r'Conditional conclusion', 'DEFAULT'),
+            (r'Today I learned', 'MANE_PARAGRAPH_DECLARATION'),
+            (r'multiplied with', 'MULTIPLICATION_INFIX'),
+            (r'the product of(?=.+?)(?![.,!?‽…:])(?=.+?)(?:(?:and)|(?:by))', 'MULTIPLICATION_PREFIX'),
+            (r'I remembered', 'RUN_PARAGRAPH'),
+            (r'divided by', 'DIVISION_INFIX'),
+            (r'now likes?', 'VARIABLE_VALUE_ASSIGNMENT'),
+            (r'I learned', 'PARAGRAPH_DECLARATION'),
+            (r'added to', 'ADDITION_INFIX'),
+            (r'Or else', 'ELSE'),
+            (r'I wrote', 'PRINT'),
+            (r'I asked', 'PROMPT_USER_FOR_IMPORT'),
+            (r'I heard', 'READLINE'),
+            (r'I would', 'RUN_PARAGRAPH'),
+            (r'are now', 'VARIABLE_VALUE_ASSIGNMENT'),
+            (r'On the', 'CASE_PREFIX'),
+            (r'to get', 'RETURNED_VARIABLE_TYPE_DEFINITION'),
+            (r'I said', 'PRINT'),
+            (r'I sang', 'PRINT'),
+            (r'I read', 'READLINE'),
+            (r'is now', 'VARIABLE_VALUE_ASSIGNMENT'),
+            (r'(?:(?:many )|(?:the ))?sentences', 'CHAR_ARRAY_OF_ARRAYS_TYPE'),
+            (r'(?:(?:many )|(?:the ))?numbers', 'NUMBER_64_ARRAY_TYPE'),
+            (r'(?:(?:many )|(?:the ))?phrases', 'CHAR_ARRAY_OF_ARRAYS_TYPE'),
+            (r'(?:(?:an )|(?:the ))?argument', 'BOOLEAN_TYPE'),
+            (r'(?:(?:a )|(?:the ))?character', 'CHAR_TYPE'),
+            (r'(?:(?:many )|(?:the ))?quotes', 'CHAR_ARRAY_OF_ARRAYS_TYPE'),
+            (r'(?:(?:many )|(?:the ))?words', 'CHAR_ARRAY_OF_ARRAYS_TYPE'),
+            (r'(?:(?:a )|(?:the ))?sentence', 'CHAR_ARRAY_TYPE'),
+            (r'(?:(?:a )|(?:the ))?number', 'NUMBER_64_TYPE'),
+            (r'(?:(?:a )|(?:the ))?letter', 'CHAR_TYPE'),
+            (r'(?:(?:a )|(?:the ))?phrase', 'CHAR_ARRAY_TYPE'),
+            (r'(?:(?:a )|(?:the ))?quote', 'CHAR_ARRAY_TYPE'),
+            (r'(?:(?:a )|(?:the ))?word', 'CHAR_ARRAY_TYPE'),
+            (r"were(?:(?:n't)|(?: not))", 'NOT_EQUAL_INFIX'),
+            (r"had(?:(?:n't)|(?: not))", 'NOT_EQUAL_INFIX'),
+            (r"has(?:(?:n't)|(?: not))", 'NOT_EQUAL_INFIX'),
+            (r"was(?:(?:n't)|(?: not))", 'NOT_EQUAL_INFIX'),
+            (r"is(?:(?:n't)|(?: not))", 'NOT_EQUAL_INFIX'),
+            (r'(?:the )?characters', 'CHAR_ARRAY_TYPE'),
+            (r'(?:the )?letters', 'CHAR_ARRAY_TYPE'),
+            (r'(?:the )?logics', 'BOOLEAN_ARRAY_TYPE'),
+            (r'(?:the )?logic', 'BOOLEAN_TYPE'),
+            (r'Otherwise', 'ELSE'),
+            (r'becomes?', 'VARIABLE_VALUE_ASSIGNMENT'),
+            (r'subtract(?=.+?)(?![.,!?‽…:])(?=.+?)from', 'SUBTRACTION_PREFIX'),
+            (r'multiply', 'MULTIPLICATION_PREFIX'),
+            (r'without', 'SUBTRACTION_INFIX'),
+            (r'always', 'CONSTANT_INITIALIZATION'),
+            (r'divide(?=.+?)(?![.,!?‽…:])(?=.+?)(?:(?:and)|(?:by))', 'DIVISION_PREFIX'),
+            (r'either(?=.+?)(?![.,!?‽…:])(?=.+?or)', 'EXCLUSIVE_OR_PREFIX'),
+            (r'likes?', 'VARIABLE_INITIALIZATION'),
+            (r'using', 'LISTING_PARAGRAPH_PARAMETERS'),
+            (r'times', 'MULTIPLICATION_INFIX'),
+            (r'While', 'WHILE'),
+            (r'minus', 'SUBTRACTION_INFIX'),
+            (r'plus', 'ADDITION_INFIX'),
+            (r'When', 'IF_PREFIX'),
+            (r'then', 'IF_POSTFIX'),
+            (r'Dear', 'REPORT_DECLARATION'),
+            (r'with', 'RETURNED_VARIABLE_TYPE_DEFINITION'),
+            (r'were', 'EQUAL_INFIX'),
+            (r'add(?=.+?)(?![.,!?‽…:])(?=.+?)and', 'ADDITION_PREFIX'),
+            (r'add(?=.+?)(?![.,!?‽…:])(?=.+?)to', 'INCREMENT_PREFIX'),
+            (r'and', 'AND_INFIX'),
+            (r'not', 'NOT_PREFIX'),
+            (r'had', 'EQUAL_INFIX'),
+            (r'has', 'VARIABLE_INITIALIZATION-EQUAL_INFIX'),
+            (r'was', 'VARIABLE_INITIALIZATION-EQUAL_INFIX'),
+            (r'If', 'IF_PREFIX'),
+            (r'or', 'OR_INFIX'),
+            (r'is', 'VARIABLE_INITIALIZATION-EQUAL_INFIX'),
+            ]
 
 lg = LexerGenerator()
 lg.ignore(r"\s")
 lg.ignore(r"(P.)+S.+\n")
 lg.ignore(r"\(.*\)")
-for keyword in keywords.items():
+for keyword in literals:
     lg.add(keyword[1], keyword[0])
-lg.add("NAME", rf'(?:(?!{regex_is_keyword()}).)*')
+for keyword in keywords:
+    #print(keyword)
+    lg.add(keyword[1], add_boundary(keyword[0]))
+lg.add("NAME", rf'(?:(?!{regex_is_in(literals)})).(?:(?!{regex_is_in(keywords)}).)+')
 lg.add("NONSENSE", r".+?")
 lexer = lg.build()
 
@@ -212,6 +224,6 @@ Your faithful student, Twilight Sparkle.
 P.S. Twilight's drunken state truly frightened me, so I couldn't disregard her order to send you this letter. Who would have thought her first reaction to hard cider would be this... explosive? I need your advice, your help, everything, on how to deal with her drunk... self. -Spike
 """
 
-stream = lexer.lex(program3)
+stream = lexer.lex(program2)
 for i in stream:
     print(i)
